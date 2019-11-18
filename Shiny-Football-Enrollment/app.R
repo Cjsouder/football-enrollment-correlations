f339044c9d6d4dddfb5b9db21331c03388289d75
Rick Brown <rbrown@college.harvard.edu>
2019-11-18 17:12:51 -0500

    Update comments in app.R

--- Shiny-Football-Enrollment/app.R
+++ Shiny-Football-Enrollment/app.R
@@ -21,6 +21,7 @@ pub_status <- c("All", "Private", "Public")
 # Create the app's user interface
 
 ui <- fluidPage(
+  
   # Create a navbar at the top of the app
   
   navbarPage(
@@ -129,28 +130,37 @@ server <- function(input, output) {
     )
             
       output$hover_info <- renderUI({
+        
+        #
+        
         hover <- input$plot_hover
         point <- nearPoints(data_input(), hover, threshold = 5, maxpoints = 1, addDist = TRUE)
+        
+        #
+        
         if(nrow(point) == 0){return(NULL)} 
         
-        # calculate point position INSIDE the image as percent of total dimensions
-        # from left (horizontal) and from top (vertical)
+        # Calculate the percent location from the left and top sides of the
+        # window of the cursor
         
         left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
         top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
         
-        # calculate distance from left and bottom side of the picture in pixels
+        # Calculate the pixel distance from the left and bottom sides of the
+        # window of the cursor
+        
         left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
         top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
         
-        # create style property fot tooltip
-        # background color is set so tooltip is a bit transparent
-        # z-index is set so we are sure are tooltip will be on top
+        # Specify the styling and position of the panel that will appear when a
+        # user hovers over a datapoint
         
         style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
                         "left:", left_px - 200, "px; top:", top_px + 2, "px;")
         
-        # actual tooltip created as wellPanel
+        # Code what information to show on the the panel that will appear when a
+        # user hovers over a datapoint
+        
         wellPanel(
           style = style,
           p(HTML(paste0("<b> College: </b>", point$instnm, "<br/>",
@@ -163,6 +173,7 @@ server <- function(input, output) {
       
       
     output$second_selection <- renderUI({
+      
       selectInput("college", "College", sort(unique(college_data[college_data$conference == input$conf,"instnm"])))
             
     })  
@@ -217,7 +228,6 @@ server <- function(input, output) {
       
       scatter_data <- college_data %>%
         filter(instnm == input$college)
-
       
     })
     
@@ -258,28 +268,37 @@ server <- function(input, output) {
     )
     
     output$hover_scatter <- renderUI({
+      
+      #
+      
       hover <- input$plot_scat
       point <- nearPoints(data_input(), hover, threshold = 5, maxpoints = 1, addDist = TRUE)
+      
+      #
+      
       if(nrow(point) == 0){return(NULL)} 
       
-      # calculate point position INSIDE the image as percent of total dimensions
-      # from left (horizontal) and from top (vertical)
+      # Calculate the percent location from the left and top sides of the
+      # window of the cursor
       
       left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
       top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
       
-      # calculate distance from left and bottom side of the picture in pixels
+      # Calculate the pixel distance from the left and bottom sides of the
+      # window of the cursor
+      
       left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
       top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
       
-      # create style property fot tooltip
-      # background color is set so tooltip is a bit transparent
-      # z-index is set so we are sure are tooltip will be on top
+      # Specify the styling and position of the panel that will appear when a
+      # user hovers over a datapoint
       
       style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
                       "left:", left_px - 200, "px; top:", top_px + 280, "px;")
       
-      # actual tooltip created as wellPanel
+      # Code what information to show on the the panel that will appear when a
+      # user hovers over a datapoint
+      
       wellPanel(
         style = style,
         p(HTML(paste0("<b> Application Year: </b>", point$year2, "<br/>",
